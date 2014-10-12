77da2575e3efd06a59b06a4a9c069db66fd72789
Lucas Churchill <lucas@agtlucas.com>
2014-10-11 23:05:26 -0300

    Update comments.js.jsx

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -3,50 +3,50 @@
  * @jsx React.DOM
  */
 
-  /**
-   * Comment itself
-   */
-  var Comment = React.createClass({
-    render: function () {
-      return (
-        <div className='comment'>
-          <h2 className='comment-author'>
-            {this.props.author}
-          </h2>
-          <section className='comment-text'>
-            {this.props.comment}
-          </section>
-        </div>
-      );
-    }
-  });
-
-  /**
-   * Comment's list
-   */
-  var CommentList = React.createClass({
-    render: function () {
-      var commentNodes = this.props.comments.map(function (comment, index) {
-        return (
-          <Comment author={comment.author} comment={comment.comment} key={index} />
-        );
-      });
-
-      return (
-        <div className='comment-list'>
-          {commentNodes}
-        </div>
-      );
-    }
-  });
-  var fakeComments = [
-    { author: "Lucas", comment: "Hell yeah!" },
-    { author: "Lucas", comment: "Hell yeah, again!" }
-  ];
-
-  window.onload = function () {
-    React.renderComponent(
-      <CommentList comments={fakeComments} />,
-      document.getElementById('comments')
+/**
+ * Comment itself
+ */
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className='comment'>
+        <h2 className='comment-author'>
+          {this.props.author}
+        </h2>
+        <section className='comment-text'>
+          {this.props.comment}
+        </section>
+      </div>
     );
-  };
+  }
+});
+
+/**
+ * Comment's list
+ */
+var CommentList = React.createClass({
+  render: function () {
+    var commentNodes = this.props.comments.map(function (comment, index) {
+      return (
+        <Comment author={comment.author} comment={comment.comment} key={index} />
+      );
+    });
+
+    return (
+      <div className='comment-list'>
+        {commentNodes}
+      </div>
+    );
+  }
+});
+var fakeComments = [
+  { author: "Lucas", comment: "Hell yeah!" },
+  { author: "Lucas", comment: "Hell yeah, again!" }
+];
+
+window.onload = function () {
+  React.renderComponent(
+    <CommentList comments={fakeComments} />,
+    document.getElementById('comments')
+  );
+};
