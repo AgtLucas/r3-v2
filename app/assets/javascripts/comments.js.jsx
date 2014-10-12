8a293f5fa89803b7f89650a843c46e605b2efd1c
Lucas Churchill <lucas@agtlucas.com>
2014-10-12 00:07:47 -0300

    Update comments.js.jsx

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -28,6 +28,7 @@ var CommentList = React.createClass({
   render: function () {
     var commentNodes = this.props.comments.map(function (comment, index) {
       return (
+        // Comment
         <Comment author={comment.author} comment={comment.comment} key={index} />
       );
     });
